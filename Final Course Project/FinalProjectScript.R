7409987dd415dee0a29e32d6bd015f39a06d7759
rpadebet <rohit.pittu@gmail.com>
2016-10-17 20:07:21 -0400

    After completion of analysis

--- Final Course Project/FinalProjectScript.R
+++ Final Course Project/FinalProjectScript.R
@@ -9,53 +9,106 @@ library(ggplot2)
 library(R.utils)
 library(data.table)
 library(lubridate)
+library(ggthemes)
+library(plotly)
 
- ## Downloading file and decompressing
+
+## Downloading file and decompressing
 setwd("/Users/rohitpittu/R Projects/DataScience/Reproducible Research/Final Course Project/")
 url <- c("https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2")
 
-## run the following code once
+## Run the following code once
 # download.file(url,"./Data/StormData.csv.bz2",method = "curl")
 # bunzip2("./Data/StormData.csv.bz2",remove = FALSE)
 
 ## Reading into a data.table
 StormData<-fread(input = "./Data/StormData.csv",header = TRUE,strip.white = TRUE)
-str(StormData)
-tail(StormData)
 
+### Converting to Date format to allow subsetting
 StormData$BgnDate <- as_date(mdy_hms(StormData$BGN_DATE))
 StormData$EndDate <- as_date(mdy_hms(StormData$END_DATE))
 
-# Getting only post 1995 data and required columns
-StormData_96 <- StormData[year(BgnDate)>1995,.(BgnDate,EndDate,REFNUM,
+### Getting only post 1992 data
+StormData_sub <- StormData[year(BgnDate)>1992,.(BgnDate,EndDate,REFNUM,
                                                STATE,STATE__,COUNTY,COUNTYNAME,
                                                EVTYPE,FATALITIES,INJURIES,
                                                PROPDMG,PROPDMGEXP,CROPDMG,CROPDMGEXP)]
 
+Data_df<-as.data.frame(StormData_sub)
 
-source('damage.R')
-
-Data_df<-as.data.frame(StormData_96)
-
-# system.time(TOTAL_DAMAGE <- apply(Data_df[,c(11:14)],1,damage))
-
+### Computing the Total Dollar Damage per event
+source('decode.R')
 PROPEXP<-apply(Data_df[,c(11,12)],1,decoder)
 CROPEXP<-apply(Data_df[,c(13,14)],1,decoder)
 Data_df<-cbind(Data_df,PROPEXP,CROPEXP)
 Data_df <- mutate(Data_df,Tot_damage = (PROPDMG*PROPEXP)+(CROPDMG*CROPEXP))
 
-n=20
+## Top Event Types which cause the most economic damage
 
-Data_top<-Data_df%>%
+### Select the top 50 percentile of damage data
+n=50
+Data_calc<-Data_df%>%
     filter(Tot_damage > quantile(Tot_damage, prob = 1 - n/100))%>%
     select(Date=BgnDate,Ref=REFNUM,State=STATE,CountyId=COUNTY,Event=EVTYPE
            ,Damage = Tot_damage)%>%
     mutate(Event = as.factor(toupper(trim(Event))))%>%
     group_by(Event)%>%
-    summarize(EventDamage = sum(Damage))%>%
+    summarize(EventDamage = sum(Damage)/1000000000)%>%
     arrange(desc(EventDamage))
 
-barplot(Data_top[10,]$EventDamage,horiz = TRUE,col=Data_top[10,]$Event)
+### Select top 10 Event Types
+Data_top10<-Data_calc[c(1:10),]
+
+### Plot the results
+g1<-ggplot(Data_top10,aes(x=factor(Event),y=EventDamage,fill=Event))+
+    geom_bar(stat="identity",col="black",width=0.5)+
+    coord_flip()+
+    guides(fill=FALSE)+
+    ylab("Economic Damage(in $ billions)")+
+    xlab("Top 10 Event Types")+
+    ggtitle("Most Economically Damaging Event Types")+
+    theme_gdocs()
+print(g1)
+
+## Yearly Economic Damage
+Data_year<-Data_df%>%
+    select(Date=BgnDate,Ref=REFNUM,State=STATE,CountyId=COUNTY,Event=EVTYPE
+           ,Damage = Tot_damage)%>%
+    group_by(Year = year(Date))%>%
+    summarize(YearlyDamage = sum(Damage)/1000000000)%>%
+    arrange(Year)
+
+### Plot the results
+g2<-ggplot(Data_year,aes(x=factor(Year),y=YearlyDamage,fill=Year))+
+    geom_bar(stat="identity",col="black",width=0.5)+
+    coord_flip()+
+    guides(fill=FALSE)+
+    ylab("Economic Damage(in $ billions)")+
+    xlab("Years")+
+    ggtitle("Yearly Economic Damage")+
+    theme_gdocs()
+ggplotly(g2)
 
 
+## Top Event Types which cause the most human damage
+Data_calc<-Data_df%>%
+    select(Date=BgnDate,Ref=REFNUM,State=STATE,CountyId=COUNTY,Event=EVTYPE
+           ,FATALITIES,INJURIES)%>%
+    mutate(Event = as.factor(toupper(trim(Event))))%>%
+    group_by(Event)%>%
+    summarize(HumanDamage = sum(FATALITIES,INJURIES),Deaths = sum(FATALITIES))%>%
+    mutate(Intensity = Deaths/HumanDamage)%>%
+    arrange(desc(HumanDamage))
 
+### Select top 10 Event Types
+Data_top10<-Data_calc[c(1:10),]
+
+### Plot the results
+g3<-ggplot(Data_top10,aes(x=factor(Event),y=HumanDamage,fill=Intensity))+
+    geom_bar(stat="identity",col="black",width=0.5)+
+    coord_flip()+
+    ylab("Human Damage(Fatalities+Injuries)")+
+    xlab("Top 10 Event Types")+
+    ggtitle("Event types causing most human damage")+
+    theme_fivethirtyeight()
+ggplotly(g3)
--- Final Course Project/decode.R
+++ Final Course Project/decode.R
@@ -1,7 +1,38 @@
+
+# Function used to decode the EXP in the dataset
+decoder<-function(v){
+    
+    if(length(v[2])==0){return (1)}
+    else if(v[2]%in% c("H","h")) {return(100)}
+    else if(v[2]%in% c("K","k")) {return(1000)}
+    else if(v[2]%in% c("M","m")) {return(1000000)}
+    else if(v[2]%in% c("B","b")) {return(1000000000)}
+    else if(v[2]%in% c("0","1","2","3","4","5","6","7","8")) {10^as.numeric(v[2])}
+    else if(v[2]=="") {return(1)} 
+    else return(1)
+}
+
+## Function to compute total Dollar Damage
+
+damage<-function(v){
+    total = 0
+    
+    PROPDMG <- ifelse(length(v['PROPDMG'])==0,0,as.numeric(v['PROPDMG']))
+    CROPDMG <- ifelse(length(v['CROPDMG'])==0,0,as.numeric(v['CROPDMG']))
+    
+    PEXP <- decode(v['PROPDMGEXP'])
+    CEXP <- decode(v['CROPDMGEXP'])
+    
+    total = total + (PROPDMG*PEXP)+(CROPDMG*CEXP)
+    
+    return(total)
+}
+
+# Decode function used in Damage function
 decode<-function(inp){
     if(length(inp)==0){
         return (1)
-        }
+    }
     else{
         key <- c("K","M","B","0","")
         value<- c(1000,1000000,1000000000,1,1)
@@ -12,15 +43,3 @@ decode<-function(inp){
     }
     
 }
-
-decoder<-function(v){
-    
-    if(length(v[2])==0){return (1)}
-    else if(v[2]=="K") {return(1000)}
-    else if(v[2]=="M") {return(1000000)}
-    else if(v[2]=="B") {return(1000000000)}
-    else if(v[2]=="0") {return(1)}
-    else if(v[2]=="") {return(1)} 
-    else return(1)
-    }
-    
